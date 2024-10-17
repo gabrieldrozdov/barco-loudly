9bca182635f1fc9f29a25ca741a52da880bf7762
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2024-10-17 17:41:30 -0400

    changed overall design

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const title = document.querySelector('.title');
 let movement = {};
 let temp = '';
 let index = 0;
-let colors = ['#70b4f8', '#f5c3cb', '#ff9e2e', '#f61d5d', '#70f892', '#cfcfcf'];
 
 // Generate letter spans
 for (let letter of title.innerText) {
@@ -17,7 +16,6 @@ for (let letter of title.innerText) {
 		'speed': Math.random()*3+1,
 		'directionRot': Math.round(Math.random()),
 		'rot': 0,
-		'color': colors[Math.floor(Math.random()*colors.length)],
 	};
 
 	index++;
@@ -65,30 +63,16 @@ function titleAnimation() {
 		const letterRect = letter.getBoundingClientRect();
 
 		if (letterRect.left < artworkRect.left-10) {
-			if (movement[key]['directionX'] == 0) {
-				movement[key]['color'] = colors[Math.floor(Math.random()*colors.length)];
-			}
 			movement[key]['directionX'] = 1;
 		} else if (letterRect.right > artworkRect.right+10) {
-			if (movement[key]['directionX'] == 1) {
-				movement[key]['color'] = colors[Math.floor(Math.random()*colors.length)];
-			}
 			movement[key]['directionX'] = 0;
 		}
 
 		if (letterRect.top < artworkRect.top-10) {
-			if (movement[key]['directionY'] == 0) {
-				movement[key]['color'] = colors[Math.floor(Math.random()*colors.length)];
-			}
 			movement[key]['directionY'] = 1;
 		} else if (letterRect.bottom > artworkRect.bottom+10) {
-			if (movement[key]['directionY'] == 1) {
-				movement[key]['color'] = colors[Math.floor(Math.random()*colors.length)];
-			}
 			movement[key]['directionY'] = 0;
 		}
-
-		letter.style.color = movement[key]['color'];
 	}
 
 	requestAnimationFrame(titleAnimation);
@@ -120,35 +104,5 @@ function nudgeLetter(letter) {
 		movement[key]['directionRot'] = 0;
 	}
 
-	movement[key]['speed'] = Math.random()*3+1,
-	movement[key]['color'] = colors[Math.floor(Math.random()*colors.length)];
-}
-
-// Voice sampler ("Animalese")
-const voiceSamplerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
-const notes = ['C3', 'E3', 'G3'];
-let voiceSamplers = {}
-for (let letter of voiceSamplerLetters) {
-	voiceSamplers[letter] = new Tone.Sampler({
-		urls: {
-			C2: `voice-${letter}.mp3`
-		},
-		baseUrl: "assets/voice/",
-		volume: -5,
-	}).toDestination();
-}
-function playVoice(letter) {
-	letter = letter.toLowerCase();
-	if (letter != " " && voiceSamplerLetters.includes(letter)) {
-		voiceSamplers[letter].triggerAttackRelease(notes[Math.floor(Math.random()*notes.length)], 1);
-	}
-}
-
-// Add hover effects for audio
-document.addEventListener('click', addSoundEffects);
-function addSoundEffects() {
-	document.removeEventListener('click', addSoundEffects);
-	for (let letter of titleLetters) {
-		letter.addEventListener('mouseenter', () => {playVoice(letter.innerText)});
-	}
+	movement[key]['speed'] = Math.random()*3+1;
 }
